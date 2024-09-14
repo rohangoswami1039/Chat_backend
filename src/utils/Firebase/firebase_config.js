f17599cb01743ccb5661baf23c6cd34f5353b6a9
rahul goswami <rohangoswami1039@gmail.com>
2024-09-14 22:44:48 +0530

    Changes in firebase config json

--- src/utils/Firebase/firebase_config.js
+++ src/utils/Firebase/firebase_config.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 //const serviceAccount = require("./firebase_sdk.json");
 
-const serviceAccount = require("./etc/secrets/firebase_sdk.json");
+const serviceAccount = require("../../../etc/secrets/firebase_sdk.json");
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
